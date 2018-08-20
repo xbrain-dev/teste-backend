0e1f2a7f0d758e02183aa715bc13814c6792378a
Luis Gustavo <agateownz@outlook.com>
2018-08-20 20:32:16 -0300

    Altera o tipo da busca por datas no repositório de Vendas.
    - Documenta os services de Venda e Vendedor.

--- teste-xbrain/src/main/java/com/lgdias/testexbrain/service/VendaService.java
+++ teste-xbrain/src/main/java/com/lgdias/testexbrain/service/VendaService.java
@@ -6,7 +6,21 @@ import java.util.List;
 
 public interface VendaService {
 
+  /**
+   * Calcula a média diária de vendas.
+   *
+   * @param vendas Lista de vendas realizadas
+   * @param dias Número de dias
+   * @return A média diária de vendas
+   */
   Double calcularMediaVendas(List<Venda> vendas, long dias);
 
+  /**
+   * Realiza uma nova venda.
+   *
+   * @param venda A venda para ser realizada
+   * @return A venda concretizada
+   * @throws VendedorNaoEncontradoException Se o vendedor não for encontrado
+   */
   Venda salvar(Venda venda) throws VendedorNaoEncontradoException;
 }
--- teste-xbrain/src/main/java/com/lgdias/testexbrain/service/VendedorService.java
+++ teste-xbrain/src/main/java/com/lgdias/testexbrain/service/VendedorService.java
@@ -2,11 +2,29 @@ package com.lgdias.testexbrain.service;
 
 import com.lgdias.testexbrain.dto.VendedorRankingDTO;
 import com.lgdias.testexbrain.exception.DataInvalidaException;
+import com.lgdias.testexbrain.model.Vendedor;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface VendedorService {
 
-  List<VendedorRankingDTO> recuperarRankingPorVendasNoPeriodo(Long number, LocalDate initialDate, LocalDate finalDate)
+  /**
+   * Recupera um ranking dos vendedores com melhor média diária de vendas no período informado.
+   *
+   * @param limiteRanking Número de vendedeores para limitar
+   * @param dataInicial Data de início do período
+   * @param dataFinal Data de conclusão do período
+   * @return Lista de vendedores juntamente com sua média de vendas diária
+   */
+  List<VendedorRankingDTO> recuperarRankingPorVendasNoPeriodo(Long limiteRanking,
+      LocalDate dataInicial, LocalDate dataFinal)
       throws DataInvalidaException;
+
+  /**
+   * Cadastra um novo vendedor
+   *
+   * @param vendedor Novo vendedor
+   * @return Vendedor cadastrado
+   */
+  Vendedor salvar(Vendedor vendedor);
 }
