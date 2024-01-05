bc866f8e60ce514ec36ae70ab321f9358e5a9006
01Dri <drikill01@gmaiil.com>
2024-01-05 06:27:48 -0300

    Initializing project

---
+++ src/main/java/me/dri/TesteBackEnd/TesteBackEndApplication.java
@@ -0,0 +1,12 @@
+package me.dri.TesteBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackEndApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackEndApplication.class, args);
+	}
+
+}
